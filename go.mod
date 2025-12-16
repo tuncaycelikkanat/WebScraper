7a2d9c3b3653d74260662ab70dcb7d48a08d1111
Tuncay Çelikkanat <tuncayclkknt@gmail.com>
2025-12-16 15:36:30 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,33 @@
+module colly-chromedp-scraper
+
+go 1.25
+
+require (
+	github.com/chromedp/chromedp v0.14.2
+	github.com/gocolly/colly/v2 v2.1.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/chromedp/cdproto v0.0.0-20250803210736-d308e07a266d // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20251027170946-4849db3c2f7e // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+
+	// CLI arg check
+	if len(os.Args) < 2 || len(os.Args) > 2 {
+		fmt.Printf("the program need 1 argument but %d found.\n", len(os.Args)-1)
+		fmt.Println("usage: go run main.go <url>")
+		os.Exit(1)
+	}
+
+	targetURL := os.Args[1] //Args[0] -> main.go , Args[1] -> targetURL
+	fmt.Println("Target URL:", targetURL)
+
+	// create output folder
+	outDir, childDirName, err := createOutputDirectory(targetURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	htmlPath := filepath.Join(outDir, childDirName+".html")
+	imgPath := filepath.Join(outDir, childDirName+".png")
+
+	// fetch html
+	err = fetchHTML(targetURL, htmlPath)
+	if err != nil {
+		log.Fatal("HTML Fetch Err:", err)
+	}
+
+	// screenshot
+	err = takeScreenShot(targetURL, imgPath)
+	if err != nil {
+		log.Fatal("Screenshot Err:", err)
+	}
+
+	fmt.Println("Successfully completed.")
+}
+
+func fetchHTML(url string, outputPath string) error {
+
+	c := colly.NewCollector(
+		colly.UserAgent(
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "+
+				"(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"),
+		colly.AllowURLRevisit(),
+	)
+
+	c.SetRequestTimeout(15 * time.Second)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Fetching HTML:", r.URL.String())
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		err := os.WriteFile(outputPath, r.Body, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("HTML Saved:", outputPath)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Colly Err:", err)
+	})
+
+	return c.Visit(url)
+}
+
+func takeScreenShot(url string, outputPath string) error {
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 20*time.Second)
+	defer cancel()
+
+	var buf []byte
+
+	fmt.Println("Taking Screenshot...")
+
+	err := chromedp.Run(ctx,
+		chromedp.EmulateViewport(1920, 1080),
+		chromedp.Navigate(url),
+		chromedp.Sleep(3*time.Second),
+		chromedp.FullScreenshot(&buf, 90),
+	)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(outputPath, buf, 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Screenshot Saved:", outputPath)
+	return nil
+}
+
+func createOutputDirectory(targetURL string) (string, string, error) {
+
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return "", "", err
+	}
+
+	site := strings.ReplaceAll(u.Hostname(), "www.", "")
+	site = strings.ReplaceAll(site, ".", "_")
+
+	timestamp := time.Now().Format("2006-01-02_15-04-05")
+	childDirName := fmt.Sprintf("%s_%s", timestamp, site)
+
+	baseDir := "outputs"
+	fullDir := filepath.Join(baseDir, childDirName)
+
+	err = os.MkdirAll(fullDir, 0755)
+	if err != nil {
+		return "", "", err
+	}
+
+	return fullDir, childDirName, nil
+}
